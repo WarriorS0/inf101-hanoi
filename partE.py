71b1545b2e789d1b46d62fc309a8ed052b4b9994
WarriorS0 <67541700+WarriorS0@users.noreply.github.com>
2023-12-11 12:55:42 +0100

    Update partE.py

--- partE.py
+++ partE.py
@@ -1,7 +1,11 @@
 import pickle
 import turtle as tl
 
-def sauvScore(joueur: str, nbDisques: int, nbCoups: int, temps: int):
+###################################################################################################################
+############################### Partie E : Comparaison des scores et temps de jeu #################################
+###################################################################################################################
+
+def sauvScore(joueur: str, nbDisques: int, nbCoups: int, temps: float):
     """Stocke les données d'une partie dans le dictionnaire scores (stocké en binaire dans le fichier data)"""
 
     with open("data", "rb") as data:
@@ -20,6 +24,25 @@ def resetData():
     with open("data","wb") as data:
         pickle.dump({}, data)
 
-def afficherTemps():
-    """Affiche le temps écoulé en temps réel (depuis le lancement du programme)"""
-    tl.up()
+def reflexionMoy() -> dict:
+    """Récupère les scores et renvoie le temps moyen de réflexion par coup et par joueur"""
+    
+    with open("data","rb") as data:
+        scores = pickle.load(data)
+    
+    tempsMoy = {}
+    
+    for joueur in scores:
+        for partie in scores[joueur]:
+            tempsMoy[joueur] = round(partie[2]/partie[1])
+    
+    return tempsMoy
+            
+def afficheScores(nb:int):
+    """Affiche un tableau des meilleurs scores selon un nombre de disques donné sur la fenêtre turtle (classement en fonction du nombre de coups)"""
+    
+def afficheChronos(nb:int):
+    """Affiche un tableau des meilleurs scores selon un nombre de disques donné sur la fenêtre turtle (classement en fonction du temps de jeu)"""
+    
+def afficheReflexMoy(nb:int):
+    """Affiche un tableau des meilleurs scores selon un nombre de disques donné sur la fenêtre turtle (classement en fonction du temps de réflexion moyen)"""
