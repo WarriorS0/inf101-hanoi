a0f2b65b5cfca7d37f2dffb5ccbadef7f32ac3aa
WarriorS0 <67541700+WarriorS0@users.noreply.github.com>
2023-12-11 12:55:29 +0100

    Update partD.py

--- partD.py
+++ partD.py
@@ -1,4 +1,9 @@
 from partB import *
+import pickle
+
+###################################################################################################################
+####################################### Partie D : Annulation de coups ############################################
+###################################################################################################################
 
 def dernierCoup(coups: dict) -> tuple[int, int]:
     """Renvoie le dernier coup joué"""
@@ -15,11 +20,14 @@ def dernierCoup(coups: dict) -> tuple[int, int]:
 
 def annulerDernierCoup(coups: dict):
     """Annule le dernier coups (modifie le dictionnaire)"""
-
+    with open("cpt","rb") as cptFile:
+        cpt = pickle.load(cptFile)
+    
     dep, arr = dernierCoup(coups)
-    global cpt
     effaceDisque(coups[cpt][arr][-1], coups[cpt], len(coups[0][0]))
     del coups[cpt]
     cpt -= 1
+    with open("cpt","wb") as cptFile:
+        pickle.dump(cpt, cptFile)
     print(coups[cpt][dep])
     dessineDisque(coups[cpt][dep][-1], coups[cpt], len(coups[0][0]))
